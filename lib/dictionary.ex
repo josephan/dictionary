a6504456315f3e3e00d3fb54b81b1b23d596732c
Joseph An <joseph.an@thescore.com>
2018-02-17 01:36:36 -0500

    Initial dictionary

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,5 +1,12 @@
 defmodule Dictionary do
-  def hello do
-    IO.puts "hello world!"
+  def word_list do
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r/\n/)
+  end
+
+  def random_word do
+    word_list()
+    |> Enum.random()
   end
 end
